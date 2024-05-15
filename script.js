a4ecf32ac8a8072655648830eaa3c014fbf6bac2
Ashish Vishwakarma <148465342+coder-ashish12@users.noreply.github.com>
2024-05-16 00:27:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function playCymbalSound() {
+    var cymbalSound = document.getElementById("cymbal-sound");
+    cymbalSound.currentTime = 0; // Set playback position to start
+    cymbalSound.play();
+}
+
+function playKickSound() {
+    var cymbalSound = document.getElementById("kick-sound");
+    cymbalSound.currentTime = 0; // Set playback position to start
+    cymbalSound.play();
+}
+
+function playSnareSound() {
+    var cymbalSound = document.getElementById("snare-sound");
+    cymbalSound.currentTime = 0; // Set playback position to start
+    cymbalSound.play();
+}
+
+function playLevelSound() {
+    var cymbalSound = document.getElementById("level-sound");
+    cymbalSound.currentTime = 0; // Set playback position to start
+    cymbalSound.play();
+}
